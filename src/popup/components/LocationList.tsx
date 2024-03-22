0f7c8157be27bcc9fb64a44077c600d895781ff2
“lcc” <chengcheng.li@51hgp.com>
2024-03-22 18:03:35 +0800

    feat: active bgcolor

--- src/popup/components/LocationList.tsx
+++ src/popup/components/LocationList.tsx
@@ -4,17 +4,23 @@ import { copyText } from '~tools';
 import { useState } from 'react';
 import { Badge } from 'flowbite-react';
 
-function LocationItem({ item }: { item: CollectFrameType }){
-  const [ copySuccess, setCopySuccess ] = useState(false)
-  function copy(text: string){
+function LocationItem({ item, active, setActive }: { 
+  item: CollectFrameType,
+  setActive: () => void
+  active: Boolean,
+}){
+  function copy(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, text: string){
+    e.stopPropagation()
     copyText(text)
     setCopySuccess(true)
     setTimeout(()=>{
       setCopySuccess(false)
     },2000)
   }
+
+  const [ copySuccess, setCopySuccess ] = useState(false)
   const isGyl = item.location.pathname === "/gyl/"
-  
+
   const params = []
   const searchStr = item.location.href.split('?')[1]
   if(searchStr){
@@ -29,30 +35,51 @@ function LocationItem({ item }: { item: CollectFrameType }){
   console.log(111, params);
 
   return (
-    <li className=" flex items-center">
-      <div className='flex-1 w-0 flex'>
+    <li className={` hover:bg-slate-200 rounded-md px-1 ${active ? ' bg-slate-200' : ''}`}>
+      <section className=" flex items-center" onClick={ setActive }>
+        <div className='flex-1 w-0 flex'>
+          {
+            isGyl
+            ? <Badge size='xs' color='success' className='mr-2'>GYL</Badge>
+            : <Badge size='xs' color='failure' className='mr-2'>ERP</Badge> 
+          }
+          <a title={item.location.href} className='font-medium text-sm truncate'>{ item.tabName }</a>
+        </div>
+        <div className=' ml-2'>
+          <a title='复制hash' onClick={(e) => copy(e, item.location.hash)}>
+            <CtIcon icon={copySuccess ? "check" : "copy"}></CtIcon>
+          </a>
+        </div>
+      </section>
+
+      <section >
         {
-          isGyl
-          ? <Badge size='xs' color='success' className='mr-2'>GYL</Badge>
-          : <Badge size='xs' color='failure' className='mr-2'>ERP</Badge> 
+          params.map((param) => {
+            return <span>{ param.key }: { param.value}</span>
+          })
         }
-        <a title={item.location.href} className='font-medium text-sm truncate'>{ item.tabName }</a>
-      </div>
-      <div className=' ml-2'>
-        <a title='复制hash' onClick={() => copy(item.location.hash)}>
-          <CtIcon icon={copySuccess ? "check" : "copy"}></CtIcon>
-        </a>
-      </div>
+      </section>
     </li>
   )
 }
 
 function LocationList({ data }: { data: CollectFrameType[]}){
+  const [activeIdx, setActiveIdx] = useState(-1)
   return (
     <ul className=" w-80">
       {
         data.map((item, index) => {
-          return <LocationItem item={item} key={index}></LocationItem>
+          return (
+            <LocationItem 
+              item={item} 
+              key={index} 
+              active={index === activeIdx}
+              setActive={() => {
+                console.log(2222)
+                setActiveIdx(index)
+              }}
+            ></LocationItem>
+          )
         })
       }
     </ul>
