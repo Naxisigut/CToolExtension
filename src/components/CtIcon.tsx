b6c9d060f739cf19d08daf211c7ca0c0a50f046c
“lcc” <chengcheng.li@51hgp.com>
2024-03-26 18:09:15 +0800

    style: 参数tag布局

--- src/components/CtIcon.tsx
+++ src/components/CtIcon.tsx
@@ -3,7 +3,7 @@ function CtIcon({ icon }){
   switch (icon) {
     case 'copy':
       iconEle = (
-        <svg className="w-6 h-6 text-gray-800 dark:text-white hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <svg className="inline w-6 h-6 text-gray-800 dark:text-white hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
           <path fillRule="evenodd" d="M18 3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1V9a4 4 0 0 0-4-4h-3a1.99 1.99 0 0 0-1 .267V5a2 2 0 0 1 2-2h7Z" clipRule="evenodd"/>
           <path fillRule="evenodd" d="M8 7.054V11H4.2a2 2 0 0 1 .281-.432l2.46-2.87A2 2 0 0 1 8 7.054ZM10 7v4a2 2 0 0 1-2 2H4v6a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3Z" clipRule="evenodd"/>
         </svg>
@@ -11,7 +11,7 @@ function CtIcon({ icon }){
       break;
     case 'check':
       iconEle = (
-        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <svg className="inline w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11.917 9.724 16.5 19 7.5"/>
         </svg>
       )
--- src/popup/components/LocationList.tsx
+++ src/popup/components/LocationList.tsx
@@ -4,65 +4,7 @@ import { copyText } from '~tools';
 import { useState } from 'react';
 import { Badge } from 'flowbite-react';
 
-function LocationItem({ item, active, setActive }: { 
-  item: CollectFrameType,
-  setActive: () => void
-  active: Boolean,
-}){
-  function copy(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, text: string){
-    e.stopPropagation()
-    copyText(text)
-    setCopySuccess(true)
-    setTimeout(()=>{
-      setCopySuccess(false)
-    },2000)
-  }
-
-  const [ copySuccess, setCopySuccess ] = useState(false)
-  const isGyl = item.location.pathname === "/gyl/"
-
-  const params = []
-  const searchStr = item.location.href.split('?')[1]
-  if(searchStr){
-    const obj = new URLSearchParams(searchStr)
-    obj.forEach((v, k) =>{
-      params.push({
-        key: k,
-        value: v
-      })
-    })
-  }
-  console.log(111, params);
-
-  return (
-    <li className={` hover:bg-slate-200 rounded-md px-1 ${active ? ' bg-slate-200' : ''}`}>
-      <section className=" flex items-center" onClick={ setActive }>
-        <div className='flex-1 w-0 flex'>
-          {
-            isGyl
-            ? <Badge size='xs' color='success' className='mr-2'>GYL</Badge>
-            : <Badge size='xs' color='failure' className='mr-2'>ERP</Badge> 
-          }
-          <a title={item.location.href} className='font-medium text-sm truncate'>{ item.tabName }</a>
-        </div>
-        <div className=' ml-2'>
-          <a title='复制hash' onClick={(e) => copy(e, item.location.hash)}>
-            <CtIcon icon={copySuccess ? "check" : "copy"}></CtIcon>
-          </a>
-        </div>
-      </section>
-
-      <section >
-        {
-          params.map((param) => {
-            return <span>{ param.key }: { param.value}</span>
-          })
-        }
-      </section>
-    </li>
-  )
-}
-
+/* 列表组件 */
 function LocationList({ data }: { data: CollectFrameType[]}){
   const [activeIdx, setActiveIdx] = useState(-1)
   return (
@@ -75,8 +17,7 @@ function LocationList({ data }: { data: CollectFrameType[]}){
               key={index} 
               active={index === activeIdx}
               setActive={() => {
-                console.log(2222)
-                setActiveIdx(index)
+                setActiveIdx(activeIdx === index ? -1 : index)
               }}
             ></LocationItem>
           )
@@ -85,5 +26,106 @@ function LocationList({ data }: { data: CollectFrameType[]}){
     </ul>
   )
 }
+
+/* 列表项组件 */
+function LocationItem({ item, active, setActive }: { 
+  item: CollectFrameType,
+  setActive: () => void
+  active: Boolean,
+}){
+  console.log(0, item);
+  /* 复制hash */
+  const [ copySuccess, setCopySuccess ] = useState(false)
+  function copy(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, text: string){
+    e.stopPropagation()
+    copyText(text)
+    setCopySuccess(true)
+    setTimeout(()=>{
+      setCopySuccess(false)
+    },1000)
+  }
+
+  /* 新旧页面标识 */
+  const isGyl = item.location.pathname === "/gyl/"
+
+  /* 参数 */
+  const params = []
+  const searchStr = item.location.href.split('?')[1]
+  if(searchStr){
+    const obj = new URLSearchParams(searchStr)
+    obj.forEach((value, key) =>{
+      params.push({key, value})
+    })
+  }
+
+  /* 路径 */
+  function getPath(location: Location){
+    let res = ''
+    const { href, origin } = location
+    res = href.split(origin)[1]
+    res = res.split('?')[0]
+    return res
+  }
+  const path = getPath(item.location)
+  console.log(222, path);
+
+  return (
+    <li className={` rounded-md overflow-hidden p-1 hover:bg-slate-200 ${active ? ' bg-slate-200' : ''}`}>
+      <section className={` flex items-center`} onClick={ setActive }>
+        <div className='flex-1 w-0 flex'>
+          {
+            isGyl
+            ? <Badge size='xs' color='success' className='mr-2'>GYL</Badge>
+            : <Badge size='xs' color='failure' className='mr-2'>ERP</Badge> 
+          }
+          <a title={item.location.href} className='font-medium text-sm truncate'>{ item.tabName }</a>
+        </div>
+        <div className=' ml-2'>
+          <a className=' inline-block' title='复制hash' onClick={(e) => copy(e, item.location.hash)}>
+            <CtIcon icon={copySuccess ? "check" : "copy"}></CtIcon>
+          </a>
+          <a className=' inline-block' title='复制文件路径' onClick={(e) => copy(e, item.location.hash)}>
+            <CtIcon icon={copySuccess ? "check" : "copy"}></CtIcon>
+          </a>
+        </div>
+      </section>
+
+      {
+        active && (
+          <section className=' p-1'>
+            <div className=' h-px bg-slate-400'></div>
+            <div className=' text-sm'>
+              <h5 className='inline-block w-[50px]'>PATH: </h5>
+              <p className='inline'>{ path }</p>
+            </div>
+            <div className=' text-sm'>
+              <h5 className='inline-block w-[50px]'>FILE: </h5>
+              <p className='inline'>{  }</p>
+            </div>
+            <div className=' flex flex-wrap mt-1'>
+              {
+                params.map((param) => {
+                  return <ParamTag tag={param}></ParamTag>
+                })
+              }
+            </div>
+          </section>
+        ) 
+      }
+    </li>
+  )
+}
+
+function ParamTag({tag}: {
+  tag: { key: String, value: String }
+}){
+  return (
+    <span className='rounded-full bg-red-300 px-2 py-1 mr-1 mb-1'>
+      <span className=' font-bold'>{ tag.key }: </span>
+      <span>{ tag.value }</span>
+    </span>
+  )
+}
+
 
 export default LocationList
